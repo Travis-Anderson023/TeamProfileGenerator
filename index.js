190b849c53028a9815c48ab67ddc79819744f6a1
Spotexx <tsanderson.023@gmail.com>
2022-02-13 01:45:17 -0500

    general push

--- index.js
+++ index.js
@@ -10,120 +10,88 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-inquirer
-    .prompt([
+let teamMembers = [];
+let teamCards = [];
+
+let addTeamMember = async (i) => {
+    inquirer.prompt([
         {
-            type: "input",
-            message: "What is manager 1's name?",
-            name: "managerName",
+            type: "list",
+            name: "role",
+            message: "What type of team member would you like to add?",
+            choices: ["Manager", "Engineer", "Intern"]
         },
         {
             type: "input",
-            message: "What is manager 1's id?",
-            name: "managerId",
+            message: `What is the team members name?`,
+            name: `name${i}`,
         },
         {
             type: "input",
-            message: "What is manager 1's email?",
-            name: "managerEmail",
+            message: `What is the team member's id?`,
+            name: `id${i}`,
         },
         {
             type: "input",
-            message: "What is manager 1's office number?",
-            name: "managerOfficeNumber",
+            message: `What is the team member's email?`,
+            name: `email${i}`,
         },
+        //if manager ask manager quetions
         {
             type: "input",
-            message: "What is Engineer 1's name?",
-            name: "engineer1Name",
+            message: `What is the team members office number?`,
+            when: ({ role }) => {
+                if (role === "Manager") {
+                    return true;
+                } return false;
+            },
+            name: `officeNumber${i}`,
         },
+        //if engineer ask engineer questions
         {
             type: "input",
-            message: "What is Engineer 1's id?",
-            name: "engineer1Id",
+            message: `What is the team members github?`,
+            when: ({ role }) => {
+                if (role === "Engineer") {
+                    return true;
+                } return false;
+            },
+            name: `github${i}`,
         },
+        //if intern ask intern questions
         {
             type: "input",
-            message: "What is Engineer 1's email?",
-            name: "engineer1Email",
+            message: `What is the team members school?`,
+            when: ({ role }) => {
+                if (role === "Intern") {
+                    return true;
+                } return false;
+            },
+            name: `school${i}`,
         },
-        {
-            type: "input",
-            message: "What is Engineer 1's github?",
-            name: "engineer1Github",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 2's name?",
-            name: "engineer2Name",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 2's id?",
-            name: "engineer2Id",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 2's email?",
-            name: "engineer2Email",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 2's github?",
-            name: "engineer2Github",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 3's name?",
-            name: "engineer3Name",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 3's id?",
-            name: "engineer3Id",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 3's email?",
-            name: "engineer3Email",
-        },
-        {
-            type: "input",
-            message: "What is Engineer 3's github?",
-            name: "engineer3Github",
-        },
-        {
-            type: "input",
-            message: "What is Intern 1's name?",
-            name: "intern1Name",
-        },
-        {
-            type: "input",
-            message: "What is Intern 1's id?",
-            name: "intern1Id",
-        },
-        {
-            type: "input",
-            message: "What is Intern 1's email?",
-            name: "intern1Email",
-        },
-        {
-            type: "input",
-            message: "What is Intern 1's school?",
-            name: "intern1School",
+    ]).then(({ role, name, id, email, officeNumber, github, school }) => {
+        switch (role) {
+            case "Manager":
+                let manager = new Manager(name, id, email, officeNumber);
+                teamCards.push(manager);
+                break;
+            case "Engineer":
+                let engineer = new Engineer(name, id, email, github);
+                teamCards.push(engineer);
+                break;
+            case "Intern":
+                let intern = new Intern(name, id, email, school);
+                teamCards.push(intern);
+                break;
         }
-    ])
-    .then(function (response) {
-        let manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber);
-        let engineer1Name = new Engineer(response.engineer1Name, response.engineer1Id, response.engineer1Email, response.engineer1Github);
-        let engineer2Name = new Engineer(response.engineer2Name, response.engineer2Id, response.engineer2Email, response.engineer2Github);
-        let engineer3Name = new Engineer(response.engineer3Name, response.engineer3Id, response.engineer3Email, response.engineer3Github);
-        let intern1Name = new Intern(response.intern1Name, response.intern1Id, response.intern1Email, response.intern1School);
+        return;
+    });
+};
 
-        let team = [manager, engineer1Name, engineer2Name, engineer3Name, intern1Name];
 
-        let html =
-            `
+let generateHTML = (teamMembers) => {
+    let html =
+        `
             <!DOCTYPE html>
             <html lang="en">
             
@@ -141,101 +109,37 @@ inquirer
                 </nav>
                 <main>
                     <!-- card -->
-                    <div class="card">
-                        <header>
-                            <h2>${team[0].getName()}</h2>
-                            <h3>${team[0].getRole()}</h3>
-                        </header>
-                        <section>
-                            <div>
-                                <p>ID: ${team[0].getId()}</p>
-                            </div>
-                            <div>
-                                <p>Email: ${team[0].getEmail()}</p>
-                            </div>
-                            <div>
-                                <p>Office Number: ${team[0].getOfficeNumber()}</p>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="card">
-                        <header>
-                            <h2>${team[1].getName()}</h2>
-                            <h3>${team[1].getRole()}</h3>
-                        </header>
-                        <section>
-                            <div>
-                                <p>ID: ${team[1].getId()}</p>
-                            </div>
-                            <div>
-                                <p>Email: ${team[1].getEmail()}</p>
-                            </div>
-                            <div>
-                                <p>Github: ${team[1].getGithubLink()}</p>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="card">
-                        <header>
-                            <h2>${team[2].getName()}</h2>
-                            <h3>${team[2].getRole()}</h3>
-                        </header>
-                        <section>
-                            <div>
-                                <p>ID: ${team[2].getId()}</p>
-                            </div>
-                            <div>
-                                <p>Email: ${team[2].getEmail()}</p>
-                            </div>
-                            <div>
-                                <p>Github: ${team[2].getGithubLink()}</p>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="card">
-                        <header>
-                            <h2>${team[3].getName()}</h2>
-                            <h3>${team[3].getRole()}</h3>
-                        </header>
-                        <section>
-                            <div>
-                                <p>ID: ${team[3].getId()}</p>
-                            </div>
-                            <div>
-                                <p>Email: ${team[3].getEmail()}</p>
-                            </div>
-                            <div>
-                                <p>Github: ${team[3].getGithubLink()}</p>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="card">
-                        <header>
-                            <h2>${team[4].getName()}</h2>
-                            <h3>${team[4].getRole()}</h3>
-                        </header>
-                        <section>
-                            <div>
-                                <p>ID: ${team[4].getId()}</p>
-                            </div>
-                            <div>
-                                <p>Email: ${team[4].getEmail()}</p>
-                            </div>
-                            <div>
-                                <p>Github: ${team[4].getSchool()}</p>
-                            </div>
-                        </section>
-                    </div>
-            
+                    ${teamCards}
                 </main>
             </body>
             
             </html>
         `;
-        fs.writeFile("./dist/team.html", html, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Success!");
-        });
+    fs.writeFile("./dist/team.html", html, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
     });
+};
+
+
+let createTeamMembersArray = async () => {
+    const answers= await inquirer.prompt([
+        //would you like to add another teammember?
+        {
+            type: "list",
+            name: "addAnother",
+            message: "Would you like to add another team member?",
+            choices: ["Yes", "No"]
+        }
+    ])
+
+        if (answers.addAnother === "Yes") {
+            addTeamMember(teamMembers.length + 1);
+            createTeamMembersArray();
+        } else {
+            generateHTML(teamMembers);
+        }
+};
+createTeamMembersArray();
